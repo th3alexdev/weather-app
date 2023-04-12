b0408675f61e11151ef1cc0a77b4dbf74bc1a79c
AleKs <alexanderfranciscog@gmail.com>
2023-04-12 02:40:06 -0400

    improving section structures

--- src/components/layout/Section/Section.jsx
+++ src/components/layout/Section/Section.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import Card from '../../Card/Card'
+import React from "react";
+import Card from "../../Card/Card";
 
 function Section({ title }) {
   return (
-    <section>
-      <h1>{ title }</h1>
-      <div className="card-container">
-        <Card title="Visibility"/>
-        <Card title="Humadity"/>
-        <Card title="Real Feel"/>      
-        <Card title="UV Index"/>
-      </div>        
+    <section className="today-highlight">
+      <h1 className="title title--principal">{ title }</h1>
+
+      <div className="cards-container">
+
+        <Card title="Visibility"
+              info={`5.2`}
+              className="card" />
+
+        <Card title="Humadity"
+              info="87"
+              className="card" />
+
+        <Card title="Real Feel"
+              info="29"
+              className="card" />
+
+        <Card title="UV Index"
+              info="3"
+              className="card" />
+
+      </div>
     </section>
-  )
+  );
 }
 
-export default Section
+export default Section;

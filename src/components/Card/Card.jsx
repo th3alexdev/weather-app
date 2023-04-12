0812bc2504e3d14a3b59330c1a72df1dbf981d69
AleKs <alexanderfranciscog@gmail.com>
2023-04-12 02:38:07 -0400

    'add additional text' function was created

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+function addAdditionalText(title) {
+  let text
+
+  if(title == "Visibility") {
+    text = ' km'
+  } else if(title == "Humadity") {
+    text = '%'
+  } else if(title == "Real Feel") {
+    text = ' ºC'
+  } else {
+    text = '';
+  }
+
+  return text
+}
+
 function Card({ title, info }) {
   return (
-    <div>
-      <p>uwu</p>
+    <div className='card'>
+      <h2 className='title title--secondary'>{ title }</h2>
+      <p className='text text--data'>{ `${info}${addAdditionalText(title)}`}</p>
     </div>
   )
 }

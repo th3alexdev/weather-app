e896f1af1347f446ceb48bc7cd7fab5f021f7c5d
AleKs <alexanderfranciscog@gmail.com>
2023-04-11 23:48:09 -0400

    inputHandler was moved to other file

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -52,26 +52,4 @@ const getCoords = async(cityName) => { // Función para obtener las coordenadas 
   }
   
   import { getCoords, getWeather } from "./weatherAPI";
-
-  const handleInput = async( cityName ) => { // Función que maneja la entrada
-      // del usuario
-  try {
-  let coords, data;
-  
-  coords = await getCoords(cityName); // Se obtienen las coordenadas
-  data = await getWeather(coords); // Se obtiene la información del clima usando las coordenadas.
-  
-  console.log(data)
-  
-  } catch(error) {
-  console.error('There was an error handling the input', error);
-  }
-  
-  }
-  
-  /* test */
-  //   let cityName = "London";
-  //   handleInput( cityName )
-  
-  export default handleInput;
   

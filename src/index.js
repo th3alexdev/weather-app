92ac76aa78c7bcad5a0fde5d801439fe901c4290
AleKs <alexanderfranciscog@gmail.com>
2023-04-13 13:54:45 -0400

    more imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export { default as Card } from "./components/Card/Card"
 export { default as Section } from "./components/layout/Section"
 
+export { default as TodayHighlightsCard } from "./components/Card/TodayHighlightsCard";
+export { default as TodayForecastCard } from "./components/Card/TodayForecastCard";
+export { default as WeatherCard } from "./components/Card/WeatherCard"
 

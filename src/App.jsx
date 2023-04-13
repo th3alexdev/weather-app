1faad9fc586cb6d4fbce03d4f8eb7d32cbba4ffc
AleKs <alexanderfranciscog@gmail.com>
2023-04-13 13:51:59 -0400

    App structure changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {
         Card, 
-        Section
+        Section,
+        WeatherCard,
+        TodayHighlightsCard,
+        TodayForecastCard        
        } from './index'
 
 import "./scss/styles.scss"
@@ -9,13 +12,39 @@ function App() {
 
   return (
     <main className='main'>
-      
-      <Section title="Today's Highlights">
+
+      <Section>
+        <WeatherCard className="card card--principal"/>
+        <p>main section</p>
       </Section>
-      
+
+      <Section>
+        <p>search section</p>
+      </Section>
+
+      <Section title="Today's Highlights">
+        <p>highlights summary</p>
+      </Section>
+
+      <Section title="Today's Forecast">
+        <p>forecast summary</p>
+      </Section>
+
+      <Section>
+        <p> other cards + devcard</p>
+      </Section>
+
     </main>
   )
 }
 
 export default App
+
 {/* <Card  />   */}
+      
+{/* <Section 
+title="Today's Highlights"
+>
+</Section>
+<Section title="Today's Forecast">
+</Section> */}

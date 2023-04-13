9e7810c92c7499398de99cf5a8fa9a60b1987110
AleKs <alexanderfranciscog@gmail.com>
2023-04-13 19:05:09 -0400

    new exports to App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-export { default as Card } from "./components/Card/Card"
+// export { default as Card } from "./components/Card/Card"
+export { default as Header } from "./components/layout/Header" 
 export { default as Section } from "./components/layout/Section"
 
+export { default as WeatherCard } from "./components/Card/WeatherCard"
+export { default as SearchCard} from "./components/Card/SearchCard"
 export { default as TodayHighlightsCard } from "./components/Card/TodayHighlightsCard";
 export { default as TodayForecastCard } from "./components/Card/TodayForecastCard";
-export { default as WeatherCard } from "./components/Card/WeatherCard"
 

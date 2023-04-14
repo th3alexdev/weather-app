561aca7f8e4f7ab6c6fa92f323247f6464b01f06
AleKs <alexanderfranciscog@gmail.com>
2023-04-14 07:46:02 -0400

    Header component structure prototype

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
-function Header() {
+function Header({ className }) {
   return (
-    <h1>header!</h1>
+
+    <header className= { className }>
+
+      <div className="header__greeting">
+      
+        <h1 class="header-text-principal">Hello,</h1>
+        
+        <div className="name-container">
+            <div className="greeting">
+              <h2 id="name" spellCheck="false" contentEditable="false" className="header-text-secondary">Stranger</h2>
+            </div>
+        </div>
+
+      </div>
+
+      <div className="header__btns">
+        <button className="btn btn--header">F</button>
+        <button className="btn btn--header">C</button>
+      </div>
+    </header>
+
 )
 }
 

6c5fc8af895d1ca08e4b26af83c892da9d75625e
AleKs <alexanderfranciscog@gmail.com>
2023-04-14 11:05:34 -0400

    forecast's flex cards

--- src/components/Card/TodayForecastCard/TodayForecastCard.jsx
+++ src/components/Card/TodayForecastCard/TodayForecastCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import img from "../../../assets/icons/weather/sunny.png"
 
 function TodayForecastCard() {
+
   return (
     <>
-      <h1>Today's Forecasts</h1>
+     <div className="section-container section-container--today-forecast">
+
+        <div className="weather-fc-cards-container">
+
+          <div className="weather-fc-card">
+            <h2 className="title weather-fc-card__title">Sun</h2> 
+            <img src={ img } className="img weather-fc-card__img"/>
+            <p className="text weather-fc-card__temperature">15º</p>
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }

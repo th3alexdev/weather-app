cf88a76bc12e89eca3d8784312407b7429c7e668
AleKs <alexanderfranciscog@gmail.com>
2023-04-14 07:58:07 -0400

    Section component structure was improved

--- src/components/layout/Section/Section.jsx
+++ src/components/layout/Section/Section.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import Card from "../../Card/Card";
 
-function Section({ title }) {
+function Section({ title, className, children}) {
   return (
     <section className="today-highlight">
-      <h1 className="title title--principal">{ title }</h1>
+      {
+        title ? <h1 className="title title--principal">{ title }</h1> : null
+      }
+      
+      { children }
 
-      <div className="cards-container">
-
-        <Card title="Visibility"
-              info={`5.2`}
-              className="card" />
-
-        <Card title="Humadity"
-              info="87"
-              className="card" />
-
-        <Card title="Real Feel"
-              info="29"
-              className="card" />
-
-        <Card title="UV Index"
-              info="3"
-              className="card" />
-
-      </div>
     </section>
   );
 }
 
 export default Section;
+
+// <div className="cards-container">
+// 
+// <Card title="Visibility"
+//       info={`5.2`}
+//       className="card" />
+// 
+// <Card title="Humadity"
+//       info="87"
+//       className="card" />
+// 
+// <Card title="Real Feel"
+//       info="29"
+//       className="card" />
+// 
+// <Card title="UV Index"
+//       info="3"
+//       className="card" />
+// 
+// </div>

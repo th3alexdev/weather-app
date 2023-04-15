14f57589dbf8a113b9ee41248062857399de2962
AleKs <alexanderfranciscog@gmail.com>
2023-04-15 15:58:29 -0400

    new components added and restructured

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import {
           WeatherCard,
           SearchCard,
           TodayHighlightsCard,
-          TodayForecastCard        
+          TodayForecastCard,
+          ExtraCard  
         } from './index'
 
 import "./scss/styles.scss"
@@ -15,19 +16,34 @@ function App() {
   return (
     <>
       <main className='main'>
-        <Header className="header" />
+        <div className="main-container">
 
-        <Section className="weather main-section">
-          <WeatherCard/>
-        </Section>
+          <Header className="header" />
 
-        <Section className="search main-section">
-          <SearchCard/>
-        </Section>
+          <Section className="weather main-section main-section--weather">
+            <WeatherCard/>
+          </Section>
 
-        <Section className="today-higlight main-section">
-          <TodayHighlightsCard />
-        </Section>
+          <Section className="search main-section">
+            <SearchCard/>
+          </Section>
+
+          <Section className="today-highlight main-section" title="Today's Highlights">
+            <TodayHighlightsCard />
+          </Section>
+
+          <div className="secondary-container">
+            <Section className="today-forecast main-section" title="Today's Forecast">
+              <TodayForecastCard />
+            </Section>
+
+            <Section className="extra main-section">
+              <ExtraCard/>
+            </Section>
+            
+          </div>
+
+        </div>
       </main>
     </>
   )

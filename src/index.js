278559b92b62cb0895de87029bf5fb0fedb9269a
AleKs <alexanderfranciscog@gmail.com>
2023-04-15 14:53:42 -0400

    added ExtraCard file to export to App

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ export { default as WeatherCard } from "./components/Card/WeatherCard"
 export { default as SearchCard} from "./components/Card/SearchCard"
 export { default as TodayHighlightsCard } from "./components/Card/TodayHighlightsCard";
 export { default as TodayForecastCard } from "./components/Card/TodayForecastCard";
+export { default as ExtraCard} from "./components/Card/ExtraCard"
 

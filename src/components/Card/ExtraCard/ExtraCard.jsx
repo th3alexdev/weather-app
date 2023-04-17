29241c80a9cd1345df14785859607e94c0bb36ef
AleKs <alexanderfranciscog@gmail.com>
2023-04-17 00:18:20 -0400

    ExtraCard has been restructured

--- src/components/Card/ExtraCard/ExtraCard.jsx
+++ src/components/Card/ExtraCard/ExtraCard.jsx
@@ -2,31 +2,65 @@ import React from 'react'
 
 function ExtraCard() {
 
+  const icon = "../../../../src/assets/icons/arrow.svg";
   const img = "https://avatars.githubusercontent.com/u/55007470?s=400&u=3ab0176605b70fce349ab80b7cdcfb47e35a6e6e&v=4";
 
   return (
-    <div className="extra-cards-container">
+    <div className="cards-container cards-container--others">
 
-      <div className="extra-card">
-        <h2 className="title subtitle">Sunrise & Sunset</h2> 
+      <div className="other-card other-card--sunrise-sunset">
+
+
+          <h2 className="title card-title">Sunrise & Sunset</h2> 
+
+          <div className="other-card__box-time">
+              <div className="icon-time-box">
+                  <img src={ icon } alt="sunrise icon" className="icon-time"/>
+              </div>
+              <p className="other-card-time__text">6:35 AM</p>
+          </div>
+          <div className="other-card__box-time">
+                <div className="icon-time-box">
+                    <img src={ icon } alt="sunset icon" className="icon-time icon-time--sunset"/>
+                </div>
+                <p className="other-card-time__text">5:42 PM</p>
+          </div>
       </div>
       
-      <div className="extra-card">
-        <h2 className="title subtitle">Air Quality</h2>
-        <p className="text text--info">105</p> 
+      <div className="other-card">
+        <h2 className="title card-title">Air Quality</h2>
+        <p className="text--data">105</p> 
       </div>
-      
-      <div className="extra-card extra-card--dev">
-        <h2 className="title subtitle">Dev</h2>
-        <p className="text text-dev">Alexander Pérez</p>
 
-        <div className="extra-card-box">
-          <p className="text text-dev"><a className="link link--dev" href="#">@th3alexdev</a></p>
+      <div className="other-card other-card--dev">
+        <h2 className="title card-title">Dev</h2>
+        <p className="other-card-dev__text">Alexander Pérez</p>
+
+        <div className="highlight-box">
+            <p className="other-card-dev__text">
+              <a className="link link--dev" target="_blank" href="https://github.com/th3alexdev/">@th3alexdev</a>
+            </p>
         </div>
 
-        <p className="text text-dev text-dev--follow">Follow this repo!</p>
+        <p>Follow this repo!</p>
 
-        <img className="img img--dev" src={ img } alt="developer github profile's image" />
+        <img 
+          src={ img } 
+          alt="developer github profile's image"
+          className="img img--dev" 
+        />
+
+        <img 
+          src="../../../../src/assets/icons/cloud-icon.webp"
+          alt="cloud icon"
+          className="img other-card-dev other-card-dev--cloud"
+        />
+
+        <img
+          src="../../../../src/assets/icons/sun-icon.webp"
+          alt="sun icon"
+          className="img other-card-dev other-card-dev--sun"
+        />
       </div>
 
     </div>

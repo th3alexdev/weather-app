81df807cd01bdc6228444ab560a63df9ff63a1e5
AleKs <alexanderfranciscog@gmail.com>
2023-05-10 22:02:46 -0400

    ARIA tags added + import paths for icons updated

--- src/components/Card/ExtraCard/ExtraCard.jsx
+++ src/components/Card/ExtraCard/ExtraCard.jsx
@@ -1,55 +1,59 @@
 import React from 'react'
 import { getSunriseAndSunset } from '../../../utils/convertUnixTimestamp';
 
-import windIcon from "../../../assets/icons/wind-solid.svg"
+import arrowIcon from "../../../../src/assets/icons/arrow.svg";
+import windIcon from "../../../../src/assets/icons/wind-solid.svg"
+import cloudIconBg from "../../../../src/assets/icons/cloud-icon.webp";
+import sunIconBg from "../../../../src/assets/icons/sun-icon.webp";
+
 
 function ExtraCard({ weather }) {
 
   let windSpeed = weather.wind.speed * 3.6
 
-  const icon = "../../../../src/assets/icons/arrow.svg";
   const img = "https://avatars.githubusercontent.com/u/55007470?s=400&u=3ab0176605b70fce349ab80b7cdcfb47e35a6e6e&v=4";
 
   return (
-    <div className="cards-container cards-container--others">
-
-      <div className="other-card other-card--sunrise-sunset">
+    <div className="cards-container cards-container--others" role="region" aria-label="Other Information">
+      <div className="other-card other-card--sunrise-sunset" aria-label="Sunrise & Sunset">
           <h2 className="title card-title">Sunrise & Sunset</h2> 
           <div className="card-flex">
               <div className="icon-time-box">
-                  <img src={ icon } alt="sunrise icon" className="icon-time"/>
+                  <img src={ arrowIcon } alt="sunrise icon" className="icon-time"/>
               </div>
               <p className="other-card-time__text">{ `${getSunriseAndSunset(weather.sys.sunrise)} AM` }</p>
           </div>
           <div className="card-flex">
                 <div className="icon-time-box">
-                    <img src={ icon } alt="sunset icon" className="icon-time icon-time--sunset"/>
+                    <img src={ arrowIcon } alt="sunset icon" className="icon-time icon-time--sunset"/>
                 </div>
                 <p className="other-card-time__text">{ `${getSunriseAndSunset(weather.sys.sunset)} PM` }</p>
           </div>
       </div>
       
-      <div className="other-card">
+      <div className="other-card" role="region" aria-label="Wind Speed">
         <h2 className="title card-title">Wind Speed</h2>
         <div className="card-flex">
-          <img src={ windIcon } alt="" className="icon icon-card"/>
+          <img src={ windIcon } alt="Wind icon" className="icon icon-card" aria-hidden="true"/>
           <p className="card-text card-text--large">{ windSpeed.toFixed(1) }
-            <span className="subtext"> km</span>
+            <span className="subtext">km</span>
           </p> 
         </div>
       </div>
 
-      <div className="other-card other-card--dev">
+      <div className="other-card other-card--dev"  aria-label="Developer Information">
         <h2 className="title card-title">Dev</h2>
         <p className="other-card-dev__text">Alexander Pérez</p>
 
         <div className="highlight-box">
             <p className="other-card-dev__text">
-              <a className="link link--dev" target="_blank" href="https://github.com/th3alexdev/">@th3alexdev</a>
+              <a className="link link--dev" target="_blank"  rel="noopener noreferrer" href="https://github.com/th3alexdev/">@th3alexdev</a>
             </p>
         </div>
 
-        <p>Follow this repo!</p>
+        <p>
+          <a className="link-repo" href="https://github.com/th3alexdev/weather-app" rel="noopener noreferrer" target="_blank">Follow this repo!</a>
+        </p>
 
         <img 
           src={ img } 
@@ -58,30 +62,21 @@ function ExtraCard({ weather }) {
         />
 
         <img 
-          src="../../../../src/assets/icons/cloud-icon.webp"
+          src={ cloudIconBg }
           alt="cloud icon"
           className="img other-card-dev other-card-dev--cloud"
+          aria-hidden="true"
         />
 
         <img
-          src="../../../../src/assets/icons/sun-icon.webp"
+          src={ sunIconBg }
           alt="sun icon"
           className="img other-card-dev other-card-dev--sun"
+          aria-hidden="true"
         />
       </div>
-
     </div>
   )
 }
-
-// <div className="weather-fc-cards-container">
-
-// <div className="weather-fc-card">
-//   <h2 className="title weather-fc-card__title">Sun</h2> 
-//   <img src={ img } className="img weather-fc-card__img"/>
-//   <p className="text weather-fc-card__temperature">15º</p>
-// </div>
-
-// </div>
 
 export default ExtraCard

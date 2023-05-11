f787e6bad97630c052f0aaea71f55a64b9a3a01a
AleKs <alexanderfranciscog@gmail.com>
2023-05-10 22:22:31 -0400

    import statements removed and ARIA tags added +

--- src/components/WeatherDashboard/WeatherDashboard.jsx
+++ src/components/WeatherDashboard/WeatherDashboard.jsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import {
   Section,
-  WeatherCard,
-  SearchCard,
-  TodayHighlightsCard,
   TodayForecastCard,
   ExtraCard,
   Loader,
@@ -15,7 +12,13 @@ import converTo from "../../utils/convertDegrees"
 import setNextDays from '../../utils/nextDays'
 import { convertUnixTimeStamp } from '../../utils/convertUnixTimestamp'
 
+/* Import imgs */
+import emptyIllustration from "../../../src/assets/img/empty.png";
+import scheduleIcon from "../../../src/assets/icons/schedule.svg";
+import calendarMonthIcon from "../../../src/assets/icons/calendar-month.svg";
+
 function WeatherDashboard({ weather, showData, loadingData, degreeType, unsplashImg, forecast}) {
+ 
   let description, weatherIcon, weatherUrl, nextDays, fulldate;
 
   if(showData) {
@@ -24,19 +27,16 @@ function WeatherDashboard({ weather, showData, loadingData, degreeType, unsplash
     description = description.join(" ");
 
     weatherIcon = weather.weather[0].icon;
-    weatherUrl = `../../../public/weather/${weatherIcon}.png`
+    weatherUrl = `/weather/${weatherIcon}.png`
 
     nextDays = setNextDays()
-
-    console.log(weather)
 
     fulldate = convertUnixTimeStamp(weather.dt, weather.timezone)
   }
 
   return (
     <>
-      {/* <main className={ showData ? "main" : "main main--home-page" }> */}
-      <main className="main">
+      <main className="main" role="main">
         <div className={ showData ? "principal principal--not-empty" : "principal"}>
 
           { loadingData && <Loader/> }
@@ -44,92 +44,105 @@ function WeatherDashboard({ weather, showData, loadingData, degreeType, unsplash
           {
             showData ? (
               <>
-                <Section className="weather section-layout">
-                  <img className="img img--weather" src={ showData && weatherUrl  } alt={ showData && weather.weather[0]}/>
+                <Section className="weather section-layout" aria="Weather Section">
+                  <img 
+                    className="img img--weather"
+                    src={ showData && weatherUrl  }
+                    alt={`Icon for ${weather.weather[0].main}`}
+                    aria-hidden="true"
+                  />
                   <Card subtitle="Weather" extra="section-container--weather">
                     <div className="weather__properties">
-                      <p className="text text--degrees"> { converTo(weather.main.temp, degreeType) }
+                      <p className="text text--degrees" aria-live="polite"> 
+                        { converTo(weather.main.temp, degreeType) }
                         <span className="degrees degrees--main">{ degreeType == "celsius" ? "ºC" : "ºF" }</span> 
                       </p>
-                      <p className="text text--w-info"> { showData && description} </p>
-                      <p className="text text--w-info">{`Rain -  ${Math.round(forecast.list[0].pop * 100) }%`}</p>
+                      <p className="text text--w-info" aria-live="polite">{ showData && description}</p>
+                      <p className="text text--w-info" aria-live="polite">{`Rain -  ${Math.round(forecast.list[0].pop * 100) }%`}</p>
                     </div>
                   </Card>
-                  
                 </Section>
     
-                <Section className="search section-layout">
+                <Section className="search section-layout" aria="About Section">
                   <Card extra="section-container--search">
-                      <div className="search-date">
+                      <div className="search-date" aria-label="Current date and time">
                         <div className="search-date-container">
-                          <img src="../../../src/assets/icons/schedule.svg" alt="" className="icon" />
-                          <p className="search-date__main-text">{`${fulldate.weekDay},`} </p>
-                          <p className="search-date__secondary-text">{`${fulldate.time}:${fulldate.minutes} `} </p>
+                          <img src={ scheduleIcon } alt="Clock icon" className="icon" />
+                          <p className="search-date__main-text" aria-live="polite">{`${fulldate.weekDay},`} </p>
+                          <p className="search-date__secondary-text" aria-live="polite">{`${fulldate.time}:${fulldate.minutes} `}</p>
                         </div>
                         <div className="search-date-container">
-                          <img src="../../../src/assets/icons/calendar-month.svg" alt="" className="icon" />
-                          <p className="search-date__main-text">{`${fulldate.month} ${fulldate.day}${fulldate.susffix},`} </p>
-                          <p className="search-date__secondary-text">{`${fulldate.year}`} </p>
+                          <img src={ calendarMonthIcon } alt="Calendar icon" className="icon" />
+                          <p className="search-date__main-text" aria-live="polite">{`${fulldate.month} ${fulldate.day}${fulldate.susffix},`} </p>
+                          <p className="search-date__secondary-text" aria-live="polite">{`${fulldate.year}`} </p>
                         </div>
                       </div>
-                      <hr className="hr"/>
-                      <div className="search-location">
-                        <p className="text search-location__text">{`${weather.name}, ${weather.sys.country}`}</p>
-                        <div className="search-location__shadow"></div>
-                        <img src={ unsplashImg } className="img img--location" alt="Image of the city searched to know the current climate"/> 
+                      <hr className="hr" aria-hidden="true"/>
+                      <div className="search-location" aria-label="Search location">
+                        <p className="text search-location__text" aria-live="polite">{`${weather.name}, ${weather.sys.country}`}</p>
+                        <div className="search-location__shadow" aria-live="polite" aria-hidden="true"></div>
+                        <img src={ unsplashImg } className="img img--location" alt="Image of the city searched to know the current climate" aria-hidden="true"/> 
                       </div>
                   </Card>
                 </Section>
 
-                <Section className="today-highlight" title="Today's Highlights">
+                <Section className="today-highlight" title="Today's Highlights" aria="Today's Highlights Section">
                   <Card extra="section-container--today-highlight">
                     
                   <div className="cards-container cards-container--th">
-                      <div className="th-card">
+                      <div className="th-card" aria-label="Real Feel">
                         <h2 className="title card-title">Real Feel</h2>
-                        <p className='text card-text degrees'>{ converTo(weather.main.feels_like, degreeType) }
+                        <p className='text card-text degrees' aria-live="polite">
+                          { converTo(weather.main.feels_like, degreeType) }
                           <span className="degrees-sup">{ degreeType == "celsius" ? "ºC" : "ºF" }</span> 
                         </p>
                       </div>
 
-                      <div className="th-card">
+                      <div className="th-card" aria-label="Humidity">
                         <h2 className="title card-title">Humidity</h2>
-                        <p className='text card-text'>{ weather.main.humidity }
+                        <p className='text card-text'aria-live="polite">
+                          { weather.main.humidity }
                           <span>%</span>
                         </p>
                       </div>
 
-                      <div className="th-card">
+                      <div className="th-card" aria-label="Max & Min Temperature">
                         <h2 className="title card-title">Max & Min</h2>
                         <div className="card-flex card-flex--small">
-                          <p className='text card-text card-text--large'>{ converTo(forecast.list[0].main.temp_max, degreeType) }º</p>
-                          <p className='text card-text card-text--secondary'>/{ converTo(forecast.list[0].main.temp_min, degreeType) }º</p>
+                          <p className='text card-text card-text--large' aria-live="polite">
+                            { converTo(forecast.list[0].main.temp_max, degreeType) }º</p>
+                          <p className='text card-text card-text--secondary' aria-live="polite">
+                            /{ converTo(forecast.list[0].main.temp_min, degreeType) }º</p>
                         </div>
                       </div>
 
-                      <div className="th-card">
+                      <div className="th-card" aria-label="Pressure">
                         <h2 className="title card-title">Pressure</h2>
-                        <p className='text card-text'>{ weather.main.pressure}
+                        <p className='text card-text' aria-live="polite">
+                          { weather.main.pressure}
                           <span className="subtext subtext--large"> mbar</span>
                         </p>
                       </div>
                   </div>
                   </Card>
-
                 </Section>
 
                 <div className="secondary-container">
-                  <Section className="today-forecast main-section" title="Week's Forecast">
+                  <Section className="today-forecast main-section" title="Week's Forecast" aria="Week's Forecast">
                     
-                    <TodayForecastCard>
+                    <TodayForecastCard role="list">
                       {
                         forecast.list.map((card, key) => (
 
                           key === 0 ? null : (
 
-                            <div key={key} className="fc-card">
-                            <h2 className="title fc-card__title">{ nextDays[key] }</h2> 
-                            <img src={`../../../public/weather/${card.weather[0].icon}.png`} className="img fc-card__img"/>
+                          <div key={key} className="fc-card" role="listitem" aria-label={`Forecast for ${nextDays[key].long}`}>
+                            <h2 className="title fc-card__title">{ nextDays[key].short }</h2> 
+                            <img 
+                              src={`/weather/${card.weather[0].icon}.png`}
+                              className="img fc-card__img"
+                              alt={`Icon for ${card.weather[0].description}`}
+                            />
                             <div className="text fc-card__temperature">
                               <p className='fc-card-max-temp'>{ converTo(card.main.temp_max, degreeType) }º</p>
                               <p className='fc-card-min-temp'>{ converTo(card.main.temp_min, degreeType) }º</p>
@@ -150,19 +163,17 @@ function WeatherDashboard({ weather, showData, loadingData, degreeType, unsplash
 
               </>
 
-            ) : ( !loadingData && (<div className="main-centered">
-                    <div className="empty-container">
-                      <img src="../../../src/assets/img/empty.png" alt="" className="img empty__img" />
-                      <p className="empty__text">Empty for now</p>
-                    </div>
-                  </div> ))
+            ) : ( !loadingData && (
+                <div className="main-centered" role="main">
+                  <div className="empty-container">
+                    <img src={ emptyIllustration } alt="Empty illustration" className="img empty__img" role="presentation"/>
+                    <p className="empty__text">Empty for now</p>
+                  </div>
+                 </div> 
+                 ))
 
 
           }
-
-
-
-
         </div>
       </main>
     </>

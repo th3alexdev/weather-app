3a82d0c43114dc09595a805b2fb2d16652593f02
AleKs <alexanderfranciscog@gmail.com>
2023-05-11 08:00:54 -0400

    double quotes were changed to single quotes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-app/",
+  base: '/weather-app/',
   preview:{
     port: 1002
   },

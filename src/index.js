c60148cd7afbd6d82cb7d49c4b7e6d9efda1babe
AleKs <alexanderfranciscog@gmail.com>
2023-05-10 21:43:05 -0400

    some exports has been removed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 export { default as Header } from "./components/layout/Header" 
 export { default as Section } from "./components/layout/Section"
 
-export { default as WeatherCard } from "./components/Card/WeatherCard"
-export { default as SearchCard} from "./components/Card/SearchCard"
-export { default as TodayHighlightsCard } from "./components/Card/TodayHighlightsCard";
 export { default as TodayForecastCard } from "./components/Card/TodayForecastCard";
 export { default as ExtraCard} from "./components/Card/ExtraCard"
 export { default as Form} from "./components/Form"

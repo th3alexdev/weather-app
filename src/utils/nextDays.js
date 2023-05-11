3a38d87e0fdf1bd6146e7194bbfa92bd25ac17d6
AleKs <alexanderfranciscog@gmail.com>
2023-05-10 21:49:57 -0400

    function to calculate and return an array of next 8 days of the week improved

--- src/utils/nextDays.js
+++ src/utils/nextDays.js
@@ -1,18 +1,40 @@
 
 export default function setNextDays() {
+  const date = new Date();
+  const nextDate = new Date();
 
-    const date = new Date();
-    const nextDate = new Date();
-    const options = { weekday: 'short' };
+  let nextDayOfWeekShort, nextDayOfWeekLarge;
+  let nextDays = [];
 
-    let nextDayOfWeek;
-    let nextDays = [date];
-    
-    for (let i = 1; i < 9; i++) {
-        nextDate.setDate(date.getDate() + i);
-        nextDayOfWeek = nextDate.toLocaleDateString('en-EN',   options).split(',')[0];
-        nextDays.push(nextDayOfWeek)
-    }
+  // Get the short and long names of the current day of the week
+  nextDayOfWeekShort = date.toLocaleDateString('en-EN', {
+    weekday: 'short'
+  }).split(',')[0];
+  nextDayOfWeekLarge = date.toLocaleDateString('en-EN', {
+    weekday: 'long'
+  }).split(',')[0];
 
-    return nextDays
+  // Add the current day of the week to the nextDays array
+  nextDays.push({
+    short: nextDayOfWeekShort,
+    long: nextDayOfWeekLarge
+  });
+
+  // Calculate and add the next 8 days of the week to the nextDays array
+  for (let i = 1; i < 9; i++) {
+    nextDate.setDate(date.getDate() + i);
+    nextDayOfWeekShort = nextDate.toLocaleDateString('en-EN', {
+      weekday: 'short'
+    }).split(',')[0];
+    nextDayOfWeekLarge = nextDate.toLocaleDateString('en-EN', {
+      weekday: 'long'
+    }).split(',')[0];
+
+    nextDays.push({
+      short: nextDayOfWeekShort,
+      long: nextDayOfWeekLarge
+    });
+  }
+
+  return nextDays;
 }

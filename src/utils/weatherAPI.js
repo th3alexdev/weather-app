6586a74ebf6560b3a93b67c866829fee80dd294f
AleKs <alexanderfranciscog@gmail.com>
2023-05-10 21:46:20 -0400

    comments has been improved for function descriptions + error handling updated

--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -1,16 +1,15 @@
 const API_KEY = import.meta.env.VITE_API_KEY_OPENWEATHERMAP; // Developer API OpenWeatherMap Key
 
-const getData = async(city, coords = null) => { // Función para obtener las coordenadas de una ciudad
-  // a partir de su nombre.
-  let appid = API_KEY;
+const getData = async(city, coords = null) => { // Function to retrieve weather data for
+                                                // a city based on its name or coordinates
+  const appid = API_KEY;
 
   let url;
-  // URL para la petición de coordenadas en la API 
 
-  if(coords) {
-    console.log(coords)
-    let lat = coords.lat;
-    let lon = coords.log;
+  // If coordinates are provided, use them to fetch weather data from the API
+  if(coords) {  
+
+    const { lat, lon } = coords;
     url = `https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=${ appid }&lang=en`;
     
   } else {
@@ -19,46 +18,57 @@ const getData = async(city, coords = null) => { // Función para obtener las coo
   }
 
   try {
-    let response = await fetch(url)
-    let data = await response.json()
-    
-    console.warn('apicall')
 
+    // Make the request to the OpenWeatherMap API
+    const response = await fetch(url)
+    const data = await response.json();
+
+    // If the response data is empty or has an error code, throw an error
+    if (!data || !('cod' in data) || data.cod !== 200) {
+      const error = data;
+      throw error;
+    } 
+    
     return data;
+    
   } catch(error) {
-    console.error('There was an error', error)
+    // console.warn(error)
+    return error;
 
   }
-
 };
 
-const getForecast = async(city, coords = null) => { // Función para obtener las coordenadas de una ciudad
-  // a partir de su nombre.
-  let appid = API_KEY;
+const getForecast = async(city, coords = null) => { // Function to retrieve forecast data for a city 
+                                                    // based on its name or coordinates
+
+  const appid = API_KEY;
   let url;
 
-  // URL para la petición de coordenadas en la API 
+  // If coordinates are provided, use them to fetch forecast data from the API
   if(coords) {
-    console.log(coords)
-    let lat = coords.lat;
-    let lon = coords.log;
+    const { lat, lon } = coords;
     url = `https://api.openweathermap.org/data/2.5/forecast?lat=${ lat }&lon=${ lon }&appid=${ appid }&lang=en&cnt=8`;
-
   } else {
     url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appid}&cnt=8`;
-
   }
 
+  // Make the request to the OpenWeatherMap API
   try {
-    let response = await fetch(url)
-    let data = await response.json()
-    
+    const response = await fetch(url)
+    const data = await response.json();
+
+    // If the response data is empty or has an error code, throw an error
+    if (!data || data.cod !== "200") {
+      const error = data
+      throw error;
+    } 
+
     return data;
+    
   } catch(error) {
-    console.error('There was an error', error)
+    throw error;
 
   }
-
 };
 
 export { getData, getForecast } 

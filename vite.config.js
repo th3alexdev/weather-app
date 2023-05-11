5224c05dbcad376a6bc6a8e3e89a518227eca497
AleKs <alexanderfranciscog@gmail.com>
2023-05-10 21:43:52 -0400

    autoprefixer plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview:{
+    port: 1002
+  },
   plugins: [react()],
-  dev: {
-    port: 5001
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer()
+      ]
+    }
   }
 })
